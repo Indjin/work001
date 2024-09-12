1b6c7864e517ecee6ab069005527361186fde2d4
Rostislav <rostislav.solomakho@gmail.com>
2024-09-12 18:21:52 +0300

    iteration009

--- ftracker.go
+++ ftracker.go
@@ -106,9 +106,12 @@ func WalkingSpentCalories(action int, duration, weight, height float64) float64 
 
 	// ((0.035 * ВесСпортсменаВКг + (СредняяСкоростьВМетрахВСекунду**2 / РостВМетрах) * 0.029 * ВесСпортсменаВКг) * ВремяТренировкиВЧасах * minInH)
 
-	averSpeed := math.Pow((float64(action) * float64(lenStep)), 2)
+	//averSpeed := math.Pow((float64(action) * float64(lenStep)), 2)
 
-	return ((walkingCaloriesWeightMultiplier*weight + (averSpeed/(height/100.0))*walkingSpeedHeightMultiplier*weight) * duration * minInH)
+	averSpeed := meanSpeed(action, duration) * kmhInMsec
+
+	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow(averSpeed, 2)/(height/100.0))*walkingSpeedHeightMultiplier*weight) * duration * minInH)
+
 	//return ((float64(walkingCaloriesWeightMultiplier)*weight + (math.Pow((meanSpeed(action, duration)*kmhInMsec), 2)/height)*float64(walkingSpeedHeightMultiplier)*weight) * duration * float64(minInH))
 }
 
